04e97136ae2a076e1a64e2f7671ca03eaf79d4b5
phs8197 <pjk1245@naver.com>
2023-01-25 15:37:52 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import axios from "axios";
+
+function createMerchantCode(amount) {
+  // GET http://localhost:8080/payment/create?amount=금액입력
+}
+
+function requestPay() {
+  const IMP = window.IMP;
+  IMP.init("imp86085776");
+  const data = {
+    pg: "kcp.T0000", // PG사 (필수항목)
+    pay_method: "card", // 결제수단 (필수항목)
+    merchant_uid: `mid_${new Date().getTime()}`, // 결제금액 (필수항목)
+    name: "결제 테스트", // 주문명 (필수항목)
+    amount: "10,000", // 금액 (필수항목)
+    buyer_name: "hyunsoo", // 구매자 이름
+    buyer_tel: "010-2351-8197", // 구매자 전화번호 (필수항목)
+    buyer_email: "pjk1245@naver.com", // 구매자 이메일
+    buyer_addr: "삼전동 116-16",
+    buyer_postalcode: "13023",
+  };
+  //
+
+  IMP.request_pay(data, callback);
+}
+function callback(rsp) {
+  if (rsp.success) {
+    console.log(rsp);
+    axios({
+      url: "http://localhost:8080/payment/verify/",
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      data: {
+        imp_uid: rsp.imp_uid,
+      },
+    }).then((data) => {
+      alert("결제 완료");
+      // 서버 결제 API 성공시 로직
+    });
+  } else {
+    alert(`결제 실패 : ${rsp.error_msg}`);
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => requestPay()}>결제하기</button>
     </div>
   );
 }
